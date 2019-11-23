287b5a20d6b81bc98a237c9c72a256dac6fa861f
Lucio Pedraza Luis Alberto <llucio@funcionpublica.gob.mx>
2019-11-23 02:10:08 -0600

    :package: webpack bundle

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    },
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'firmafiel.js',
+      library: '@gobmx-sfp/firmafiel',
+      libraryTarget: 'umd'
+    }
+};
