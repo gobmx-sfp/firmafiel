0e90bb091795bc13b9c7b66cd92f5faca61b5ac6
David Regla <dreglad@gmail.com>
2020-04-17 04:43:12 -0500

    export library

---
+++ src/pdf.js
@@ -0,0 +1,31 @@
+import commonmark from 'pdfkit-commonmark';
+import CommonmarkPDFRenderer from 'pdfkit-commonmark';
+import PDFDocument from 'pdfkit';
+import { pdfkitAddPlaceholder } from 'node-signpdf';
+import { getCertificado } from './main';
+
+const writer = new CommonmarkPDFRenderer();
+const reader = new commonmark.Parser();
+
+export function crearPdf({ markdown, signaturaPlaceholder }) {
+  const pdf = new PDFDocument();
+  const parsed = reader.parse(markdown);
+
+  writer.render(pdf, parsed);
+
+  const refs = pdfkitAddPlaceholder({
+    pdf,
+    pdfBuffer: Buffer.from([pdf]),
+    ...signaturaPlaceholder,
+  });
+
+  Object.keys(refs).forEach((key) => refs[key].end());
+
+  pdf.end();
+
+  return pdf;
+}
+
+export function firmarPdf({ pdf, cert, contrasena }) {
+  return signer.sign(pdf, getCertificado(cert), { passphrase: contrasena });
+}
